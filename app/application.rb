4f1ab6eb6a81cf13fa46d39ee7f1c749db4cec94
Mark A Guza <guzamaguza@gmail.com>
2020-04-28 02:38:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,12 +4,12 @@ class Application
     resp = Rack::Response.new
     time1 = Time.new
 
-    if time1.hour < 12
+    if time1.hour.inspect < 12
       resp.write "Good Morning!"
-      #resp.write time1.hour
-    elsif time1.hour >= 12
+      resp.write time1.hour
+    elsif time1.hour.inspect >= 12
       resp.write "Good Afternoon!"
-      #resp.write time1.hour
+      resp.write time1.hour
     end
 
     resp.finish
