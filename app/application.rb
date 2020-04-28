52aef72e68e2fb13bb009fe943737233e4675b06
Mark A Guza <guzamaguza@gmail.com>
2020-04-28 02:32:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,14 +2,14 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    time1 = Time.new 
-    
-    if time1 < 12 
+    time1 = Time.new
+
+    if time1 < 12
       resp.write "Good Morning!"
-    elsif time1 >= 12 
+    elsif time1 >= 12
       resp.write "Good Afternoon!"
     end
-    
+
     resp.finish
   end
 
